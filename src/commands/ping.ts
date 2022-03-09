75631930a9613479af2c3cacca80586a80afa40d
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2022-03-09 22:47:44 +0800

    remove built-in commands

--- src/commands/ping.ts
+++
@@ -1,7 +0,0 @@
-import Command from '@structures/Command';
-
-export const command = new Command(
-	(builder) => builder.setName('ping').setDescription('Pong!'),
-	(interaction) => interaction.reply('Pong!'),
-	{},
-);
