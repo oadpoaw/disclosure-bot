e4db57e0e71795a79d021e7792ea78e1fb1f98fc
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2022-03-09 22:49:16 +0800

    make logger shard compliant

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,28 +1,30 @@
 import { createLogger } from '@oadpoaw/utils';
+import BotConfig from 'loaders/BotConfig';
 import path from 'path';
 import DailyRotateFile from 'winston-daily-rotate-file';
 
-const dailyTransport: DailyRotateFile = new DailyRotateFile({
-	filename: `logs${path.sep}%DATE%.log`,
-	datePattern: 'YYYY-MM-DD-HH-mm-ss',
-	zippedArchive: true,
-	maxSize: '20m',
-	maxFiles: '14d',
-	createSymlink: true,
-	symlinkName: 'latest.log',
-});
+const config = BotConfig();
 
-const errorTransport: DailyRotateFile = new DailyRotateFile({
-	level: 'warn',
-	filename: `logs${path.sep}errors${path.sep}%DATE%.log`,
-	datePattern: 'YYYY-MM-DD-HH-mm-ss',
-	zippedArchive: true,
-	maxSize: '20m',
-	maxFiles: '14d',
-	createSymlink: true,
-	symlinkName: 'latest.log',
-});
-
-const Logger = createLogger([dailyTransport, errorTransport]);
+const Logger = createLogger([
+	new DailyRotateFile({
+		filename: `logs${path.sep}%DATE%-${process.pid}.log`,
+		datePattern: 'YYYY-MM-DD-HH',
+		zippedArchive: true,
+		maxSize: '20m',
+		maxFiles: '14d',
+		createSymlink: !config.bot.sharding,
+		symlinkName: 'latest.log',
+	}),
+	new DailyRotateFile({
+		level: 'warn',
+		filename: `logs${path.sep}errors${path.sep}%DATE%-${process.pid}.log`,
+		datePattern: 'YYYY-MM-DD-HH',
+		zippedArchive: true,
+		maxSize: '20m',
+		maxFiles: '14d',
+		createSymlink: !config.bot.sharding,
+		symlinkName: 'latest.log',
+	}),
+]);
 
 export default Logger;
