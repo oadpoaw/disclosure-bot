a907946d296eb124ef1e202c111da0e39e470540
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2022-03-09 22:46:29 +0800

    add shardoptions

--- config.ts
+++ config.ts
@@ -1,5 +1,10 @@
-import { ClientOptions, Intents } from 'discord.js';
+import { ClientOptions, Intents, ShardingManagerOptions } from 'discord.js';
 
 export const clientOptions: ClientOptions = {
 	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+};
+
+export const shardOptions: ShardingManagerOptions = {
+	totalShards: 'auto',
+	respawn: true,
 };
