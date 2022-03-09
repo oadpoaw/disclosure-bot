735fc4483fab1a5948ba3abced697c0a54fea1fb
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2022-03-09 22:45:52 +0800

    simple ping plugin

--- plugins/Ping.ts
+++ plugins/Ping.ts
@@ -1,19 +1,30 @@
-import Plugin from '@structures/Plugin';
+import Plugin, { PluginMetaData } from '@structures/Plugin';
 
 export default class Ping extends Plugin {
-	public constructor(client: any) {
-		super(client, {
-			name: 'Ping',
-			description: 'Simple Ping Plugin',
-			version: '1.0.0',
-			author: 'oadpoaw',
-			dependencies: [],
-		});
-	}
+	public readonly metadata: PluginMetaData = {
+		name: 'Ping',
+		description: 'Simple Plugin for Ping command',
+		version: '1.0.0',
+		author: 'oadpoaw',
+		dependencies: [],
+		optionalDependencies: [],
+		npmDependencies: [],
+	};
 
 	public getDefaultConfig() {
-		return {
-			test: 'dasdasd',
-		};
+		return {};
+	}
+
+	public onLoad() {
+		this.listen('messageCreate', (_, message) => {
+			if (!message.author.bot && message.content.startsWith('?ping'))
+				message.channel.send('Pong!');
+		});
+
+		this.addCommand(
+			(builder) => builder.setName('ping').setDescription('Pong!'),
+			(interaction) => interaction.reply('Pong!'),
+			{},
+		);
 	}
 }
