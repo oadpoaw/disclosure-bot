14ca0c9a80bd4d45c829593b1b3402510e64e1ac
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2022-03-13 15:26:37 +0800

    switch to esm modules

--- scripts/postbuild-plugins.js
+++ scripts/postbuild-plugins.js
@@ -1,17 +1,12 @@
-const fs = require('fs');
-const path = require('path');
-const makePath = (p = '') => path.join(process.cwd(), 'plugins', p);
+import { readdirSync, readFileSync, writeFileSync } from 'fs';
+import { join, sep } from 'path';
+const makePath = (p = '') => join(process.cwd(), 'plugins', p);
 
-for (const file of fs.readdirSync(makePath('plugins'))) {
+for (const file of readdirSync(makePath('plugins'))) {
 	if (file.endsWith('.js')) {
-		const buffer = fs.readFileSync(makePath(`plugins${path.sep}${file}`));
-		fs.writeFileSync(
-			makePath(file),
-			buffer.toString()
-				.replace(/exports\./, `module.exports.`)
-				.replace(/module\.exports\.default/g, `module.exports`)
-		);
+		const buffer = readFileSync(makePath(`plugins${sep}${file}`));
+		writeFileSync(makePath(file), buffer.toString());
 	}
 }
 
-require('./prebuild-plugins');
+import './prebuild-plugins.js';
--- scripts/prebuild-plugins.js
+++ scripts/prebuild-plugins.js
@@ -1,8 +1,8 @@
-const rimraf = require('rimraf');
-const path = require('path');
+import rimraf from 'rimraf';
+import { join } from 'path';
 
 const log = (err) => err && console.error(err);
-const makePath = (p = '') => path.join(process.cwd(), 'plugins', p);
+const makePath = (p = '') => join(process.cwd(), 'plugins', p);
 
 rimraf(makePath('plugins'), log);
 rimraf(makePath('src'), log);
