3a01c58e10af9d45e9d2e2c27418ecb822abb2f6
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2022-03-16 14:41:34 +0800

    include tsconfig

--- scripts/init.js
+++ scripts/init.js
@@ -1,7 +1,7 @@
 import { symlinkSync, writeFileSync } from "fs";
 import { join } from "path";
 
-const json = {
+writeFileSync(join(process.cwd(), 'plugins', 'package.json'), JSON.stringify({
 	"$schema": "http://json.schemastore.org/package",
 	"private": true,
 	"type": "module",
@@ -10,7 +10,27 @@ const json = {
 	"imports": {
 		"#disclosure/*": "./dist/structures/*.js"
 	}
-};
+}, null, 4));
 
-writeFileSync(join(process.cwd(), 'plugins', 'package.json'), JSON.stringify(json, null, 4));
+writeFileSync(join(process.cwd(), 'plugins', 'tsconfig.json'), JSON.stringify({
+	"$schema": "https://json.schemastore.org/tsconfig",
+	"compilerOptions": {
+		"declaration": false,
+		"declarationMap": false,
+		"outDir": "plugins",
+		"sourceMap": false,
+		"baseUrl": "./dist",
+		"paths": {
+			"#disclosure/*": [
+				"structures/*"
+			]
+		}
+	},
+	"exclude": [],
+	"extends": "../tsconfig.json",
+	"include": [
+		"."
+	]
+}, null, 4));
+
 symlinkSync(join(process.cwd(), 'dist'), join(process.cwd(), 'plugins', 'dist'));
