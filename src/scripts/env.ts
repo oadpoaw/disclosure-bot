3cba06b3745581a72dd9a0f2b76e952cc28681ac
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2022-03-22 18:22:20 +0800

    config.js generation

--- src/scripts/env.ts
+++ src/scripts/env.ts
@@ -83,6 +83,28 @@ type BotConfiguration = ReturnType<typeof BotConfig>;
 		writeFile(['config.yml'], yaml.stringify(cfg));
 	}
 
+	writeFile(
+		['config.js'],
+		`// @ts-check
+
+import { Intents } from 'discord.js';
+
+/**
+ * @type {import('discord.js').ClientOptions}
+ */
+export const clientOptions = {
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+};
+
+/**
+ * @type {import('discord.js').ShardingManagerOptions}
+ */
+export const shardOptions = {
+	totalShards: 'auto',
+	respawn: true,
+};`,
+	);
+
 	execSync('npm run plugins:init');
 
 	console.log('Done!');
