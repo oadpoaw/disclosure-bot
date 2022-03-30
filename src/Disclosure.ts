ad8db97d2b69ee826869b6c032a157326583249e
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2022-03-30 21:56:33 +0800

    feat: terminal title styling

--- src/Disclosure.ts
+++ src/Disclosure.ts
@@ -2,6 +2,7 @@ import { Client } from './classes/client/index.js';
 import PluginLoader from './loaders/plugin/PluginLoader.js';
 import ms from 'ms';
 import { clientOptions } from '../config.js';
+import setTerminalTitle from './functions/setTerminalTitle.js';
 
 const before = Date.now();
 const client = new Client(clientOptions);
@@ -10,13 +11,13 @@ client.once('ready', async () => {
 	client.logger.info(`Authenticated.`);
 	await client.dispatcher.load();
 
-	if (!client.shard) {
-		client.logger.info(`Done! ${ms(Date.now() - before)}`);
-	}
+	client.logger.info(`Done! ${ms(Date.now() - before)}`);
+
+	setTerminalTitle(client.user?.tag || '');
 });
 
 PluginLoader(client).then(() => {
 	client.logger.info(`Authenticating...`);
 
-	client.login(client.config.bot.token);
+	client.login(client.config.token);
 });
