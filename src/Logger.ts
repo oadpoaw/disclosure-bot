681ba8b4355d11101bfb3876d4d058042fab4712
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2022-04-02 18:58:37 +0800

    feat(Logger): support sharding for no file locks

--- src/Logger.ts
+++ src/Logger.ts
@@ -2,25 +2,30 @@ import DailyRotateFile from 'winston-daily-rotate-file';
 import path from 'path';
 import { createLogger } from '@oadpoaw/utils/createLogger';
 import { processor } from '@oadpoaw/utils';
+import { Config } from './Config.js';
 
 const Logger = createLogger([
 	new DailyRotateFile({
-		filename: `logs${path.sep}%DATE%.log`,
+		filename: `logs${path.sep}%DATE%%${
+			Config.sharding ? `-${process.pid}` : ''
+		}.log`,
 		datePattern: 'YYYY-MM-DD-HH',
 		zippedArchive: true,
 		maxSize: '20m',
 		maxFiles: '14d',
-		createSymlink: true,
+		createSymlink: !Config.sharding,
 		symlinkName: 'latest.log',
 	}),
 	new DailyRotateFile({
 		level: 'warn',
-		filename: `logs${path.sep}errors${path.sep}%DATE%.log`,
+		filename: `logs${path.sep}errors${path.sep}%DATE%${
+			Config.sharding ? `-${process.pid}` : ''
+		}.log`,
 		datePattern: 'YYYY-MM-DD-HH',
 		zippedArchive: true,
 		maxSize: '20m',
 		maxFiles: '14d',
-		createSymlink: true,
+		createSymlink: !Config.sharding,
 		symlinkName: 'latest.log',
 	}),
 ]);
