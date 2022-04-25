e9e82d12df4bf23154f3b9c9c9a16c84c88da5f4
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2022-04-25 16:53:42 +0800

    feat: add shardOptions to env

--- src/scripts/env.ts
+++ src/scripts/env.ts
@@ -58,7 +58,6 @@ import type { Client } from 'discord.js';
 		['config.js'],
 		`// @ts-check
 
-
 /**
  * @type {import('discord.js').ClientOptions}
  */
@@ -66,6 +65,13 @@ export const clientOptions = {
 	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 };
 
+/**
+ * @type {import('discord.js').ShardingManagerOptions}
+ */
+export const shardOptions = {
+	totalShards: 'auto',
+	respawn: true,
+};
 `,
 	);
 
